467216f242adafa284fe4e4abbaee1b97fef4a49
Hayo <chickchok06@gmail.com>
2025-07-30 18:06:49 +0900

    .

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { BrowserRouter, Routes, Route } from "react-router";
+import "./App.css";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
